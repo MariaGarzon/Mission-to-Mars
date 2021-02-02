555549e9f697d2ae6e26905cab14835c85318c9f
Maria Garzon <garzonmm@mcmaster.ca>
2021-02-02 12:58:55 -0500

    app working

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # Use Flask to render a template
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 # Use PyMongo to interact with our Mongo database
 from flask_pymongo import PyMongo
 # To use the scraping code, we will convert from Jupyter notebook to Python
 import scraping
+#from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
+#chrome_path = r'/Users/Maria/Desktop/Mission-to-Mars/chromedriver' 
+#driver = webdriver.Chrome(executable_path=chrome_path)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -23,7 +27,8 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return redirect('/', code=302)
+   return redirect("/", code=302)
 
 if __name__ == "__main__":
-   app.run()
+    app.run
+
